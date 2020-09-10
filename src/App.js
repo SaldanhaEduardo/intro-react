1900d84b123aa8c0bbfc3ca3ca60bb389297e2c3
Eduardo Saldanha <eduardo_saldanha@hotmail.com.br>
2020-09-10 20:32:02 -0300

    button delete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
 import React, { useState, useEffect } from "react";
-import Itemlist from "./components/Itemlist";
-import List from "./components/List";
+import api from "./services/api";
 
 import "./App.css";
 
-function App() {
-  const [tasks, setTask] = useState([]);
+import Header from "./components/Header";
 
-  const addTask = (task) => {
-    setTask([...tasks, task]);
-  };
+function App() {
+  const [projects, setProjects] = useState([]);
+  const [title, setTitle] = useState("");
+  const [owner, setOwner] = useState("");
+
+  useEffect(() => {
+    api.get("/projects").then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
+    const { data: project } = await api.post("/projects", { title, owner });
+
+    setProjects([...projects, project]);
+    setTitle("");
+    setOwner("");
+  }
+
+  async function handleDelete(projectId) {
+    await api.delete(`/projects/${projectId}`);
+
+    setProjects(projects.filter((project) => project.id !== projectId));
+  }
 
   return (
-    <div className="App">
-      <>
-        <Itemlist onSave={addTask} />
-        <List tasks={tasks} />
-      </>
-    </div>
+    <>
+      <Header title="Projects" />
+
+      <ul>
+        {projects.map((project) => (
+          <li key={project.id}>
+            {project.title} - {project.owner}
+            <button onClick={() => handleDelete(project.id)}>Deletar</button>
+          </li>
+        ))}
+      </ul>
+
+      <div className="inputContainer">
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          value={owner}
+          onChange={(e) => setOwner(e.target.value)}
+        />
+      </div>
+
+      <button type="button" onClick={handleAddProject}>
+        Adicionar
+      </button>
+    </>
   );
 }
 
